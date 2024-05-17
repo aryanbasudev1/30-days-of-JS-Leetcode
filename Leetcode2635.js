2685e4a0c4ed04c0111b11978a998b7df91d3ea0
aryanbasudev1 <aryanbasudev1@gmail.com>
2024-05-17 16:11:33 +0530

    Leetcode 2635

--- Leetcode2635.js
+++ Leetcode2635.js
@@ -2,3 +2,14 @@
  fn, return a new array with a transformation applied to each element.
 The returned array should be created such that returnedArray[i] = fn(arr[i], i).
 Please solve it without the built-in Array.map method. */
+
+var map = function (arr, fn) {
+  //solution starts here
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let a = fn(arr[i], i);
+    newArr.push(a);
+  }
+  return newArr;
+  //solution ends here
+};
