3464bcdc8bbd938fe50fbe8e938be66c1a453414
aryanbasudev1 <aryanbasudev1@gmail.com>
2024-05-19 23:20:22 +0530

    Leetcode 2629

--- Leetcode2629.js
+++ Leetcode2629.js
@@ -4,3 +4,19 @@ The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
 The function composition of an empty list of functions is the identity function f(x) = x.
 You may assume each function in the array accepts one integer as input and returns one integer 
 as output. */
+
+var compose = function (functions) {
+  if (functions.length === 0) {
+    return function (x) {
+      return x;
+    };
+  } else {
+    return function (x) {
+      var result = functions[functions.length - 1](x);
+      for (var i = functions.length - 2; i >= 0; i--) {
+        result = functions[i](result);
+      }
+      return result;
+    };
+  }
+};
