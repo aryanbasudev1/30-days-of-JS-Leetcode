28f19d8bb9c09d6347e18f82091f807fd3729eb8
aryanbasudev1 <aryanbasudev1@gmail.com>
2024-05-20 21:19:02 +0530

    Leetcode 2666

--- Leetcode2666.js
+++ Leetcode2666.js
@@ -1,24 +1,14 @@
 /*Allow One function call */
 var once = function (fn) {
+  let funCalled = false;
+  let result;
   return function (...args) {
-    let call = 0;
-    if ((call = 0)) {
-      let a = function fn(...args) {
-        let sum = 0;
-        for (let i = 0; i < args.length; i++) {
-          sum += args[i];
-        }
-      };
+    if (!funCalled) {
+      result = fn(...args);
+      funCalled = true;
+      return result;
+    } else {
+      return undefined;
     }
   };
 };
-
-// let fn = (a, b, c) => a + b + c;
-
-function fn(a, b, c) {
-  return a + b + c;
-}
-let onceFn = once(fn);
-
-console.log(onceFn(1, 2, 3)); // 6
-console.log(onceFn(2, 3, 6)); // returns undefined without calling fn
